3d407234a172edd90ff9e2213e44ff282041ce97
Arthur Cheysson <arthur@cheysson.fr>
2020-03-24 19:14:16 +0100

    Version 0.2 (mauvaises dépendances)

--- backend/backend.py
+++ backend/backend.py
@@ -1,4 +1,11 @@
 from pathlib import Path
+from flit_core.buildapi import (
+    get_requires_for_build_wheel,
+    get_requires_for_build_sdist,
+    prepare_metadata_for_build_wheel,
+    build_sdist,
+    build_wheel,
+)
 
 BASE_DIR = Path(__file__).parent.parent
 SOURCE_DIR = BASE_DIR / "build" / "sources"
--- data_france/__init__.py
+++ data_france/__init__.py
@@ -1,3 +1,3 @@
 """Données administratives et géographiques sur la France"""
 
-__version__ = "0.1"
+__version__ = "0.2"
