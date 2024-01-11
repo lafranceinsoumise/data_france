59ef2c1366893e35875813b3d101b122561a8174
Salomé Cheysson <salome@cheysson.fr>
2024-01-11 21:05:41 +0100

    miniatures: simplifier tout à la fin !

--- backend/tasks/cartographie.py
+++ backend/tasks/cartographie.py
@@ -3,29 +3,21 @@ import subprocess
 import geopandas as gpd
 
 
-def generer_metropole(shp_departements, topology):
+def generer_metropole(shp_departements, geojson):
     df = gpd.read_file(shp_departements)
 
     df = df.rename(columns={"NOM": "nom", "INSEE_DEP": "code"})
-    df = df.loc[df.code.str.len() == 2, ["code", "nom", "geometry"]].to_crs(
-        2154
-    )  # LAMBERT93
+    df = df.loc[df.code.str.len() == 2, ["code", "geometry"]].to_crs(2154)  # LAMBERT93
 
-    with open(topology, "wb") as fd:
-        proc = subprocess.Popen(
-            ["geo2topo", "data=-"],
-            stdin=subprocess.PIPE,
-            stdout=fd,
-        )
-
-        proc.communicate(df.to_json().encode())
+    df.to_file(geojson, driver="GeoJSON")
 
 
 def generer_miniature(topology, departement):
     return f"""
-    topomerge data=data -f 'd.properties.code == "{departement}"' < {topology}
-    | toposimplify -P .1
-    | topo2geo data=-
-    | geoproject 'd3.geoIdentity().reflectY(true).fitSize([500,500], d)'
+    jq '.features[] | select( .properties.code == "{departement}" )' < {topology} \
+    | geoproject 'd3.geoIdentity().reflectY(true).fitSize([500,500], d)' \
+    | geo2topo data=- \
+    | toposimplify -p 64 \
+    | topo2geo data=- \
     | geo2svg -w 500 -h 500
     """
